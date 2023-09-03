8cdcee3e5c155b1ed7db6541fe3971fcc09bfc20
Bob Bytes <bytesbob@gmail.com>
2023-09-03 14:16:22 -0400

    created nxn grid of divs

---
+++ script.js
@@ -0,0 +1,21 @@
+const container = document.getElementById('container');
+const containerSize = window.getComputedStyle(container).width;
+let gridEls = [];
+
+function makeGrid(size) {
+    const elWidth = Math.floor(parseInt(containerSize)/size) + 'px';
+    console.log(elWidth);
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            let div = document.createElement('div');
+            div.classList.add('gridEl');
+            div.style.width = elWidth;
+            div.style.height = elWidth;
+            div.style.backgroundColor = 'red';
+            container.appendChild(div);
+            gridEls.push(div);
+        }
+    }
+}
+
+makeGrid(3);
