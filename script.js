9600bc5f513fb7d387ab098d1e80761b52e9198c
Bob Bytes <bytesbob@gmail.com>
2023-09-03 22:59:34 -0400

    added reset button

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const container = document.getElementById('container');
 const containerSize = window.getComputedStyle(container).width;
 let gridEls = [];
+const button = document.getElementById('reset');
+button.addEventListener('click', resetGrid);
 
 function makeGrid(size) {
     const elWidth = Math.floor(parseInt(containerSize)/size) + 'px';
@@ -18,4 +20,24 @@ function makeGrid(size) {
     }
 }
 
-makeGrid(16);
+function resetGrid() {
+    for (let i = 0; i < gridEls.length; i++) {
+        console.log('hello');
+        div = gridEls[i];
+        container.removeChild(div);
+    }
+    gridEls = [];
+    console.log(gridEls);
+    newSize = getUserInput();
+    makeGrid(newSize);
+}
+
+function getUserInput() {
+    let newSize = '';
+    newSize = prompt('Enter a new grid size between 1 and 75');
+    while (Number.isNaN(Number.parseInt(newSize)) || newSize <= 0 || newSize > 75) {
+        newSize = prompt('Invalid input. Please enter a valid number between 1 and 75');
+    }
+    return Number.parseInt(newSize);
+}
+makeGrid(8);
